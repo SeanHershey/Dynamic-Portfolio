c065cc62cdf3f41e87dde0090c376ac24db4037d
SeanHershey <shershey@calpoly.edu>
2024-04-07 12:21:18 -0700

    project photos resized on mobile

--- src/components/Photos/Photo.js
+++ src/components/Photos/Photo.js
@@ -5,10 +5,8 @@ import "./Photo.css";
 const Photo = ({ photo: { title, link, imgUrl}, index }) => {
     return (
     <div className="photo-container">
-    <a href = {link}>
         <h2 className="heading">{title}</h2>
         <img className="gallery_image" src={imgUrl} alt={title} />
-    </a>
     </div>
     );
 };
